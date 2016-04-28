0cd0ed43a5c674bde7d7d44129530a62e828bb37
touyu <akkey0222@gmail.com>
2016-04-28 15:30:11 +0900

    Initial Commit

---
+++ Collecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collecture
+//
+//  Created by Yuto Akiba on 2016/04/28.
+//  Copyright © 2016年 Yuto Akiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
